81a56593fed41ac95a4adff7f932d0ce480305c1
jldec <jurgen@jldec.me>
2025-09-16 17:38:39 -0400

    deps

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -6,7 +6,6 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({ children }) 
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="stylesheet" href={styles} />
-      <title>rwsdk-minimal-starter</title>
     </head>
     <body>
       <div id="root">{children}</div>
