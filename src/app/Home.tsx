f49b0f6c9706a5f182ccb8ce4bf57712884a8055
Jürgen Leschner <jldec@users.noreply.github.com>
2025-09-27 15:35:07 -0400

    link to GitHub (#2)

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -3,7 +3,9 @@ import { Layout } from './Layout'
 export function Home() {
   return (
     <Layout title="rwsdk-minimal-starter">
-      home
+      <a href="https://github.com/jldec/rwsdk-minimal-starter" className="text-blue-500 underline">
+        GitHub
+      </a>
     </Layout>
   )
 }
