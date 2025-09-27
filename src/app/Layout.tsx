84bc3db5217e030fcee0a9410092c83a54f30bd6
jldec <jurgen@jldec.me>
2025-09-27 15:41:58 -0400

    simplify title

--- src/app/Layout.tsx
+++ src/app/Layout.tsx
@@ -1,7 +1,7 @@
 export function Layout({ title, children }: { title: string, children: React.ReactNode }) {
   return (
     <div className="mx-auto max-w-3xl p-2">
-      <title>{`rwsdk-minimal-starter - ${title}`}</title>
+      <title>{title}</title>
       <main>
         <h1 className="text-2xl font-bold">{title}</h1>
         {children}
