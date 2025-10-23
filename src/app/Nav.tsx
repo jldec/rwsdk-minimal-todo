0730b6dc671c024f69bbebf0ec59581a1efadbf9
jldec <jurgen@jldec.me>
2025-10-23 17:21:26 +0100

    remove navigation and serve just Todo

--- src/app/Nav.tsx
+++
@@ -1,8 +0,0 @@
-export function Nav() {
-  return (
-    <nav className="flex gap-4">
-      <a href="/" className="text-blue-500 underline">Home</a>
-      <a href="/todo" className="text-blue-500 underline">Todo</a>
-    </nav>
-  )
-}
--- src/worker.tsx
+++ src/worker.tsx
@@ -2,14 +2,12 @@ import { defineApp } from 'rwsdk/worker'
 import { route, render } from 'rwsdk/router'
 
 import { Document } from '@/app/Document'
-import { Home } from '@/app/Home'
 import { TodoPage } from '@/app/TodoPage'
 
 export type AppContext = {}
 
 export default defineApp([
   render(Document, [
-    route('/', Home),
-    route('/todo', TodoPage)
+    route('/', TodoPage),
   ])
 ])
